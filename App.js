82391e436d6c25e64e278f48c33a386ae9f34e6a
kingov.alexey <kingov_alexey@mail.ru>
2023-06-14 11:18:27 +0600

    test

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const Post = styled.View`
+  flex: 1;
+  padding: 15px;
+  border-bottom-width: 5px;
+  border-bottom-color: rgba(0, 0, 0, 0, 1);
+  border-bottom-style: solid;
+`;
+
+const PostImage = styled.Image`
+  width: 100px;
+  height: 100px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!!!</Text>
-      <StatusBar style='auto' />
+    <View>
+      <Post>
+        <PostImage
+          source={{
+            uri: 'https://w7.pngwing.com/pngs/737/1015/png-transparent-man-reading-newspaper-newspaper-information-article-newspaper-ad-miscellaneous-service-photography-thumbnail.png',
+          }}
+        />
+      </Post>
+      <StatusBar theme='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
